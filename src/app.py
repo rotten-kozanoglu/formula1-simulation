7fd0b5f7dcd615a64942e66df04fd4991496fd95
m.kozanoglu <64382208+rotten-kozanoglu@users.noreply.github.com>
2024-08-15 15:20:17 +0300

    Update app.py
    son test

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ from modules.simulator import RaceSimulator, Track, Driver
 from modules.data import tracks, drivers
 import random
 import os
-# test2344
+# test234414124124
 app = Flask(__name__)
 
 race_simulator = RaceSimulator(tracks, drivers)
